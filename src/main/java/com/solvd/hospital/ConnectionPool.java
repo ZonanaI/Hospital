9c57f9b2851837fcdb0594073f675145d1224ccd
Ignacio Zonana <ignaciozonan@gmail.com>
2022-09-08 20:05:36 -0300

    Added configuration.properties file, added propiertes getter values to connection pool

--- src/main/java/com/solvd/hospital/ConnectionPool.java
+++ src/main/java/com/solvd/hospital/ConnectionPool.java
@@ -1,30 +1,35 @@
 package com.solvd.hospital;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
+
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ConnectionPool {
     private static final Logger log = LogManager.getLogger(ConnectionPool.class);
-    private final String DB="nacimientos";
-    private final String URL="jdbc:mysql://localhost:3306/"+DB+"?useUnicode=true&" +
-            "useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private final String USER="root";
-    private final String PASS="123654789";
-
+    Properties properties= new Properties();
     public static final int MAX_POOL_SIZE = 5;
 
     private static ConnectionPool dataSource;
     private BasicDataSource basicDataSource;
 
     private ConnectionPool(){
-
+        try {
+            properties.load(new FileInputStream(new File("src/main/resources/configuration.properties")));
+        } catch (IOException e){
+            log.error(e);
+        }
         basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        basicDataSource.setUsername(USER);
-        basicDataSource.setPassword(PASS);
-        basicDataSource.setUrl(URL);
+        basicDataSource.setDriverClassName(properties.getProperty("DRIVER"));
+        basicDataSource.setUsername(properties.getProperty("USER"));
+        basicDataSource.setPassword(properties.getProperty("PASS"));
+        basicDataSource.setUrl(properties.getProperty("URL"));
 
         basicDataSource.setMinIdle(1);
         basicDataSource.setMaxIdle(MAX_POOL_SIZE);
